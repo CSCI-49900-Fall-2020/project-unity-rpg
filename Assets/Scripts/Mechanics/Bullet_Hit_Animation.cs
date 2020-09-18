6ecb5c6e2b6cc04f3eb55d784cb25e5324c118f2
Phillip Chen <Philman84@gmail.com>
2020-09-18 15:30:17 -0400

    Fix a bug that wasn't destorying bullets on enemy impact added files for impact animation

---
+++ Assets/Scripts/Mechanics/Bullet_Hit_Animation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Hit_Animation : MonoBehaviour
+{
+	/*
+	public Animator animator;
+
+    // Start is called before the first frame update
+	private void OnTriggerEnter2D(Collider2D other){
+		if (other.tag =="enemy"){
+			animator.SetBool("Hit_confirm",true);
+			Destroy(this.gameObject);
+		}
+	}
+	*/
+}
