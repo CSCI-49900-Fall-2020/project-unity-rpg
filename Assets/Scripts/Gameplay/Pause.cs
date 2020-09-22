b1843755d9e5848207f6dd1ddccc048d7f65b348
Hubert Ye <hye55555@gmail.com>
2020-09-22 14:40:26 -0400

    saving progress

--- Assets/Scripts/Gameplay/Pause.cs
+++ Assets/Scripts/Gameplay/Pause.cs
@@ -7,12 +7,7 @@ public class Pause : MonoBehaviour
     // Start is called before the first frame update
 
     public static bool isPaused = false;
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
 
