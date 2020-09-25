f50f967b6b98361440fa617373708d262b506fa4
Phillip Chen <Philman84@gmail.com>
2020-09-25 18:57:52 -0400

    Allows player to shoot Left, Implemented left face projectiles, Changed files names to agreed naming convention

---
+++ Assets/Scripts/Mechanics/BulletAnimationHit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletAnimationHit : MonoBehaviour
+{
+
+	public Animator animator;
+    void Start(){
+    	animator.SetBool("hitConfirm", false);
+	}
+    // Start is called before the first frame update
+	private void OnTriggerEnter2D(Collider2D other){
+		if (other.tag =="enemy"){
+			animator.SetBool("hitConfirm",true);
+			//Destroy(this.gameObject);
+		}
+	}
+
+}
---
+++ Assets/Scripts/Mechanics/BulletShootRight.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShootRight : MonoBehaviour {
+	public float speed = 10.0f;
+	public Rigidbody2D rb;
+	public Vector2 movement;
+	private Vector2 screenBounds;
+	void Start(){
+		screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+		rb = this.GetComponent<Rigidbody2D>();
+	}
+
+	void Update(){
+		if(transform.position.x > screenBounds.x * 1.1){
+            Destroy(this.gameObject);
+        }
+		//movement = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+	}
+
+	
+	void FixedUpdate(){
+		moveCharacter(movement);
+	}
+
+	void moveCharacter(Vector2 direction){
+        	//shoots bullet at the right direction
+        	rb.MovePosition((Vector2)transform.position + new Vector2(speed * (Time.deltaTime),0f ));
+	}
+	
+}
+
