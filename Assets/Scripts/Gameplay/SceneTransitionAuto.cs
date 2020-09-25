c57f46544bddd463c9a85a5b1307fb2534fda9d1
Hubert Ye <hye55555@gmail.com>
2020-09-25 19:30:13 -0400

    saving progress

---
+++ Assets/Scripts/Gameplay/SceneTransitionAuto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionAuto : MonoBehaviour
+{
+    public string sceneName;
+    public Transform player;
+    public Vector2 spawnPoint;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        SceneManager.LoadScene(sceneName);
+        player.position = spawnPoint;
+    }
+}
