96b19c612e3e718141fc0624c1dbc6ab8b794d1c
Paolo Tejeda <paolo.t.ny@gmail.com>
2020-10-06 13:42:41 -0400

    fixing rebase

--- Assets/Scripts/Mechanics/BulletAnimationHit.cs
+++ Assets/Scripts/Mechanics/BulletAnimationHit.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using Platformer.Mechanics;
 using UnityEngine;
@@ -6,18 +6,21 @@ using UnityEngine;
 public class BulletAnimationHit : MonoBehaviour
 {
 
-	public Animator animator;
+    public Animator animator;
 
-    void Start(){
-    	animator.SetBool("hitConfirm", false);
-	}
+    void Start()
+    {
+        animator.SetBool("hitConfirm", false);
+    }
     // Start is called before the first frame update
-	private void OnTriggerEnter2D(Collider2D other){
-		if (other.tag =="enemy"){
-			animator.SetBool("hitConfirm",true);
-			
-			other.GetComponent<Collider2D>().gameObject.GetComponent<Health>().Decrement(2);
-		}
-	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "enemy")
+        {
+            animator.SetBool("hitConfirm", true);
+
+            other.GetComponent<Collider2D>().gameObject.GetComponent<Health>().Decrement(2);
+        }
+    }
 
 }
