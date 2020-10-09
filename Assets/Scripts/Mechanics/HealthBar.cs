4214fba9fc25bd6ba90744c781f879a4bf6e73d2
Hubert Ye <hye55555@gmail.com>
2020-10-09 14:14:49 -0400

    Added a lot of features

---
+++ Assets/Scripts/Mechanics/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Platformer.Mechanics
+{
+    public class HealthBar : MonoBehaviour
+    {
+        public Slider slider;
+		public Gradient gradient;
+		public Image fill;
+
+        public void SetMaxHealth(int maxHealth, int currentHealth)
+        {
+			slider.maxValue = maxHealth;
+			slider.value = currentHealth;
+
+			fill.color = gradient.Evaluate(slider.normalizedValue);
+        }
+
+        public void SetCurrentHealth(int currentHealth)
+        {
+            slider.value = currentHealth;
+			fill.color = gradient.Evaluate(slider.normalizedValue);
+        }
+    }
+}
