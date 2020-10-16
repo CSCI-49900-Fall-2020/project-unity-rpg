d5aefc6c9038c358ac3505d690340a9a9698aca8
Hubert Ye <hye55555@gmail.com>
2020-10-16 13:29:35 -0400

    added character swapping, hp and mp bars "Fixing merge conflict"

--- Assets/Scripts/Mechanics/HealthBar.cs
+++ Assets/Scripts/Mechanics/HealthBar.cs
@@ -10,17 +10,20 @@ namespace Platformer.Mechanics
         public Slider slider;
 		public Gradient gradient;
 		public Image fill;
+        public Text hpNumbers;
 
         public void SetMaxHealth(int maxHealth, int currentHealth)
         {
 			slider.maxValue = maxHealth;
 			slider.value = currentHealth;
+            hpNumbers.text = currentHealth + " / " + maxHealth;
 
 			fill.color = gradient.Evaluate(slider.normalizedValue);
         }
 
-        public void SetCurrentHealth(int currentHealth)
+        public void SetCurrentHealth(int maxHealth, int currentHealth)
         {
+            hpNumbers.text = currentHealth + " / " + maxHealth;
             slider.value = currentHealth;
 			fill.color = gradient.Evaluate(slider.normalizedValue);
         }
