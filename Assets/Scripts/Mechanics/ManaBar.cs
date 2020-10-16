7cdcfa56b1e2efa22fbe83dc3a524283501acfac
Hubert Ye <hye55555@gmail.com>
2020-10-16 13:29:35 -0400

    added character swapping, hp and mp bars

---
+++ Assets/Scripts/Mechanics/ManaBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Platformer.Mechanics
+{
+    public class ManaBar : MonoBehaviour
+    {
+            public Slider slider;
+            public Text mpNumbers;
+
+            public void SetMaxMana(int maxMana, int currentMana)
+            {
+                slider.maxValue = maxMana;
+                slider.value = currentMana;
+                mpNumbers.text = currentMana + " / " + maxMana;
+            }
+
+            public void SetCurrentHealth(int maxMana, int currentMana)
+            {
+                mpNumbers.text = currentMana + " / " + maxMana;
+                slider.value = currentMana;
+            }
+    }
+}
