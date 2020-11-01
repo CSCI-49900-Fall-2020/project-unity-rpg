83ebc78c022a35c1aa297ec2b662843775c728e2
Phillip Chen <Philman84@gmail.com>
2020-11-01 15:56:20 -0500

    added skilltree

---
+++ Assets/Scripts/Mechanics/SkillManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	public GameObject skillTreeUI;
+
+	void Update()
+	{
+	  	if(Input.GetKeyDown("t"))
+	  	{
+	    	if(skillTreeUI.activeSelf)
+	    	{
+	      		skillTreeUI.SetActive(false);
+	    	}else 
+	    	{
+	      		skillTreeUI.SetActive(true);
+	    	}
+	  	}
+	}
+}
