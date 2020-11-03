ffe34b2726f346a8ba3f26810bc50cf53ca45be0
Hubert Ye <hye55555@gmail.com>
2020-11-03 16:01:26 -0500

    saving progress "saving progress"

--- Assets/Scripts/Gameplay/UpdateManager.cs
+++ Assets/Scripts/Gameplay/UpdateManager.cs
@@ -12,7 +12,7 @@ public class UpdateManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if(gameObject.GetComponent<CharacterSwapping>().currentCharacter.GetComponent<PlayerController>().controlEnabled){
             gameObject.GetComponent<CharacterSwapping>().currentCharacter.GetComponent<PlayerAggro>().toUpdate();
