047260c002bbed7a7ebb641f1435087b0257e2d0
JamesZhaoCCC <zhaojames280@gmail.com>
2020-11-10 14:29:36 -0500

    KeyBinds.cs: Added Addkeybinds function and charchange buttons in dictionary

--- Assets/Scripts/UI/KeyBinds.cs
+++ Assets/Scripts/UI/KeyBinds.cs
@@ -10,6 +10,7 @@ namespace Platformer.UI
 
         public Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
 
+        //label all controls in the game
         public Text up, left, down, right, jump, interract, menu, inventory;
         public Text skill1, skill2, skill3, skill4;
 
@@ -18,6 +19,7 @@ namespace Platformer.UI
         private Color32 normal = new Color32(39, 171, 249, 255);
         private Color32 selected = new Color32(239, 116, 36, 255);
 
+        //display when you save or return all keys to their default binds
         private DisplayManager displayManager;
 
         // Start is called before the first frame update
@@ -25,7 +27,8 @@ namespace Platformer.UI
         {
             displayManager = DisplayManager.Instance();
 
-            keys.Add("Up", (KeyCode)System.Enum.Parse(typeof(KeyCode),PlayerPrefs.GetString("Up","W")));
+            //keys.Add(string, KeyCode)
+            keys.Add("Up", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Up", "W")));
             keys.Add("Left", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Left", "A")));
             keys.Add("Down", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Down", "S")));
             keys.Add("Right", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Right", "D")));
@@ -39,6 +42,11 @@ namespace Platformer.UI
 
             keys.Add("Interract", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Interract", "Return")));
             keys.Add("UICanvas", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("UICanvas", "Escape")));
+
+            keys.Add("Char1", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Char1", "1")));
+            keys.Add("Char2", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Char2", "2")));
+            keys.Add("Char3", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Char3", "3")));
+            keys.Add("Char4", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("Char4", "4")));
 
             up.text = keys["Up"].ToString();
             left.text = keys["Left"].ToString();
@@ -56,21 +64,34 @@ namespace Platformer.UI
             menu.text = keys["UICanvas"].ToString();
         }
 
-        // Update is called once per frame
-        void Update()
+        //function used to add keybinds to the dictionary
+        /*
+         Usage:
+          add AddKeyBind() in Start or Awake
+          replace input.getbuttondown with keyBinds.GetButtonDown("Function") //Function given in keys.Add          
+        */
+        void AddKeyBind(/*string action, string function, string defaultKey*/)//replicate KeyBinds.cs start function
         {
-            //When key is pressed, output a message
-            /*
-            if (Input.GetKeyDown(keys["Up"]))
-            {
-                Debug.Log("Up");
-            }
-            */
+            KeyBinds keyBinds;
+            keyBinds = GameObject.FindObjectOfType<KeyBinds>();
+
+            //check if entries are null or empty, if not add the keybind
+            //if (!string.IsNullOrEmpty(function) && !string.IsNullOrEmpty(defaultKey))
+            //{
+            //    keys.Add(function, (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(function, defaultKey)));
+            //}
+            //else
+            //{
+            //    Debug.Log("No keybind added");
+            //}
+            //keyBinds.GetButtonDown(function); //this line needs to be manually placed in each code
         }
-        
+
+
+        //emulate input.getbuttondown, input.getbutton, input.getbuttonup
         public bool GetButtonDown(string buttonName)
         {
-            if(keys.ContainsKey(buttonName) == false)
+            if (keys.ContainsKey(buttonName) == false)
             {
                 Debug.LogError("KeyBinds::GetButtonDown - No button called " + buttonName);
                 return false;
@@ -100,21 +121,22 @@ namespace Platformer.UI
 
             return Input.GetKeyUp(keys[buttonName]);
         }
+        //
 
         void OnGUI()
         {
-            if(currentKey != null)
+            if (currentKey != null)
             {
                 Event e = Event.current;
-                
-                if(e.isKey)
+
+                if (e.isKey)
                 {
                     keys[currentKey.name] = e.keyCode;
                     currentKey.transform.GetChild(0).GetComponent<Text>().text = e.keyCode.ToString();
                     currentKey.GetComponent<Image>().color = normal;
                     currentKey = null;
                 }
-                
+
             }
         }
 
@@ -127,7 +149,7 @@ namespace Platformer.UI
             currentKey.GetComponent<Image>().color = selected;
 
         }
-        
+
         public void SaveKeys()
         {
             foreach (var key in keys)
@@ -137,13 +159,6 @@ namespace Platformer.UI
 
             PlayerPrefs.Save();
             displayManager.DisplayMessage("Saved");
-        }
-
-        void AddKeyBind(KeyBinds keyBinds, string function, string defaultKey)
-        {
-            keyBinds = GameObject.FindObjectOfType<KeyBinds>();
-            keyBinds.GetButtonDown(function);
-            keys.Add(function, (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString(function, defaultKey)));
         }
 
         public void ReturnToDefault()
@@ -185,6 +200,18 @@ namespace Platformer.UI
 
             PlayerPrefs.Save();
             displayManager.DisplayMessage("Default Keys");
+        }
+
+        // Update is called once per frame, used for testing purposes
+        void Update()
+        {
+            //When key is pressed, output a message
+            /*
+            if (Input.GetKeyDown(keys["Up"]))
+            {
+                Debug.Log("Up");
+            }
+            */
         }
     }
 }
