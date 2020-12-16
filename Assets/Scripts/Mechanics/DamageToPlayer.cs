410d15b9d335ff45449be2436af80352495c5c3b
PaoloTejeda <paolo_t_@hotmail.com>
2020-12-16 14:28:36 -0500

    presentationpaolo

---
+++ Assets/Scripts/Mechanics/DamageToPlayer.cs
@@ -0,0 +1,19 @@
+﻿using Platformer.Mechanics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageToPlayer : MonoBehaviour
+{
+    public int damage;
+
+  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<PlayerController>().decrementHealth(damage);
+        }
+    }
+
+}
--- Assets/Scripts/Mechanics/DropItems.cs
+++ Assets/Scripts/Mechanics/DropItems.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using Platformer.Mechanics;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,22 +8,34 @@ public class DropItems : MonoBehaviour
     public bool drops;
     public GameObject theDrops;
     public Transform dropPoint;
-    public SpriteRenderer sr;
+    private SpriteRenderer sr;
+    public int numberOfHits = 0;
 
+    private void Awake()
+    {
+
+        sr = GetComponent<SpriteRenderer>();
+
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "bullet")
         {
-            Destroy(gameObject);
+            numberOfHits--;
+            if (numberOfHits == 0)
+            {
+                Debug.Log("works");
+                sr.enabled = false;
+                if (drops) Instantiate(theDrops, dropPoint.position, dropPoint.rotation);
+                Destroy(gameObject);
+            }
+            Destroy(collision.gameObject);
+
         }
     }
     public void Update()
     {
-        
-        if (sr == false)
-        { 
-            if (drops) Instantiate(theDrops,dropPoint.position,dropPoint.rotation);
-        }
+
     }
 
 }
