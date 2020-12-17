d2841e07af995f6f8d145eb4c5749be741c06739
Phillip Chen <Philman84@gmail.com>
2020-12-16 20:42:33 -0500

    skills use keybindings

--- Assets/Scripts/Mechanics/ShootManager.cs
+++ Assets/Scripts/Mechanics/ShootManager.cs
@@ -2,32 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Platformer.Mechanics;
-
+using Platformer.UI;
 public class ShootManager : MonoBehaviour
 {
     //should be merged with button manager but I want to avoid merge conflicts 
     // Start is called before the first frame update
+    KeyBinds keyBinds;
     void Start()
     {
-        
+        keyBinds = GameObject.FindObjectOfType<KeyBinds>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey ("/"))
+        if (keyBinds.GetButtonDown("Skill2"))
         {
             gameObject.GetComponent<CharacterSwapping>().currentCharacter.GetComponent<PlayerDetectShoot>().ShootBulletButton(1);
         }
-        if (Input.GetKey ("j"))
+        if (keyBinds.GetButtonDown("Skill3"))
         {
             gameObject.GetComponent<CharacterSwapping>().currentCharacter.GetComponent<PlayerDetectShoot>().ShootBulletButton(2);
         }
-        if (Input.GetKey ("k"))
+        if (keyBinds.GetButtonDown("Skill4"))
         {
             gameObject.GetComponent<CharacterSwapping>().currentCharacter.GetComponent<PlayerDetectShoot>().ShootBulletButton(3);
         }
-        if (Input.GetKey ("l"))
+        if (keyBinds.GetButtonDown("Skill1"))
         {
             gameObject.GetComponent<CharacterSwapping>().currentCharacter.GetComponent<PlayerDetectShoot>().ShootBulletButton(4);
         }
