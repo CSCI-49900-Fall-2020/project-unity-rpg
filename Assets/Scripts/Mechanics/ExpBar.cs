e895a23e62c85a8e1d08e1cc14d6a441a05fcec1
Phillip Chen <Philman84@gmail.com>
2020-12-18 02:41:02 -0500

    add exp slider

---
+++ Assets/Scripts/Mechanics/ExpBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Platformer.Mechanics
+{
+    public class ExpBar : MonoBehaviour
+    {
+            public Slider slider;
+            public Text expNumbers;
+
+            public void SetExp(int maxExp, int currentExp)
+            {
+                slider.maxValue = maxExp;
+                slider.value = currentExp;
+                expNumbers.text = currentExp + " / " + maxExp;
+            }
+
+            public void SetCurrentExp(int maxExp, int currentExp)
+            {
+                expNumbers.text = currentExp + " / " + maxExp;
+                slider.value = currentExp;
+            }
+    }
+}
--- Assets/Scripts/Mechanics/PlayerLevel.cs
+++ Assets/Scripts/Mechanics/PlayerLevel.cs
@@ -9,6 +9,7 @@ public class PlayerLevel : MonoBehaviour
 	public int playerLevel = 0;
 	public int playerExp = 0;
 	public int levelUp = 20; 
+	public GameObject slider;
 	//public GameObject skillTree;
     // Start is called before the first frame update
     void Start()
@@ -16,13 +17,20 @@ public class PlayerLevel : MonoBehaviour
         
     }
 
+    void Awake()
+    {
+    	slider.GetComponent<ExpBar>().SetExp(levelUp, playerExp);
+    }
+
     public void GainExp(int enemyExp)
     {
     	playerExp += enemyExp;
+    	slider.GetComponent<ExpBar>().SetCurrentExp(levelUp, playerExp);
     	if (playerExp >= levelUp)
     	{
     		playerExp -= levelUp;
     		levelUp += 12;
+    		slider.GetComponent<ExpBar>().SetExp(levelUp, playerExp);
     		gameObject.GetComponent<SkillTree>().abilityPoints += 1 ;
     	}
     }
